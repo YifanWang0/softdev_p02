94511ce073d43846fed09811a87bfe9296821682
Joseph Lee <jlee01@stuy.edu>
2020-01-13 09:25:07 -0500

    creating models

--- utl/models.py
+++ utl/models.py
@@ -1,6 +1,7 @@
 from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.associationproxy import association_proxy
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -8,6 +9,7 @@ class GroupLinks(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     group_id = db.Column(db.Integer, db.ForeignKey('group.id'))
+    admin = db.Column(db.Boolean, nullable = False)
 
     user = db.relationship('User', backref='groupownership')
     group = db.relationship('Group', backref='groupownership')
@@ -16,6 +18,18 @@ class GroupLinks(db.Model):
         self.user_id = user_id
         self.group_id = group_id
 
+
+class TaskLinks(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    task_id = db.Column(db.Integer, db.ForeignKey('task.id'))
+
+    user = db.relationship('User', backref='taskownership')
+    task = db.relationship('Task', backref='taskownership')
+
+    def __init__(self, user_id, task_id):
+        self.user_id = user_id
+        self.group_id = task_id
 
 class User(db.Model, UserMixin):
     # columns
@@ -38,9 +52,28 @@ class Group(db.Model):
     users = association_proxy('groupownership',
                               'user',
                               creator=lambda u: GroupLinks(u.id, id))
-    def __init__(self, id, name):
-        self.id = id
+    def __init__(self, name):
         self.name = name
 
-class Tasks(db.Model):
+class Task(db.Model):
     id = db.Column(db.Integer,primary_key=True)
+    due_date = db.Column(db.DateTime, nullable = False)
+    priority = db.Column(db.Integer, nullable = False)
+    timestamp = db.Column(db.DateTime, nullable=False,
+                                       default=datetime.utcnow)
+    title = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(80), nullable=False)
+    upvotes = db.Columnn(db.Integer, nullable = False)
+    downvotes = db.Column(db.Integer, nullable = False)
+
+    users = association_proxy('taskownership',
+                              'user',
+                              creator=lambda u: GroupLinks(u.id, id))
+
+    def __init__(self, due_date, priority, title, description):
+        self.due_date = due_date
+        self.priority = priority
+        self.title = title
+        self.description = description
+        self.upvotes = 0
+        self.downvotes = 0
