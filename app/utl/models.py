b6f5ac459297d6d366d98394a6eb65edd3092657
Joseph Lee <jlee01@stuy.edu>
2020-01-14 08:20:10 -0500

    fixing file issue

--- app/utl/models.py
+++ app/utl/models.py
@@ -1,6 +1,7 @@
 from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.associationproxy import association_proxy
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -8,6 +9,7 @@ class GroupLinks(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     group_id = db.Column(db.Integer, db.ForeignKey('group.id'))
+    credibility = db.Column(db.Integer)
 
     user = db.relationship('User', backref='groupownership')
     group = db.relationship('Group', backref='groupownership')
@@ -16,12 +18,12 @@ class GroupLinks(db.Model):
         self.user_id = user_id
         self.group_id = group_id
 
-
 class User(db.Model, UserMixin):
     # columns
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
+    tasks = db.relationship('Task', backref='user')
     groups = association_proxy('groupownership',
                               'group',
                               creator=lambda c: GroupLinks(id, c.id))
@@ -35,10 +37,32 @@ class Group(db.Model):
     # columns
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
+    tasks = db.relationship('Task', backref='group')
     users = association_proxy('groupownership',
                               'user',
                               creator=lambda u: GroupLinks(u.id, id))
-
-    def __init__(self, id, name):
-        self.id = id
+    def __init__(self, name):
         self.name = name
+
+class Task(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+    group_id = db.Column(db.Integer, db.ForeignKey('group.id'))
+
+    due_date = db.Column(db.DateTime, nullable = False)
+    priority = db.Column(db.Integer, nullable = False)
+    timestamp = db.Column(db.DateTime, nullable=False,
+                                       default=datetime.utcnow)
+    title = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(80), nullable=False)
+    upvotes = db.Columnn(db.Integer, nullable = False)
+    downvotes = db.Column(db.Integer, nullable = False)
+
+
+    def __init__(self, due_date, priority, title, description):
+        self.due_date = due_date
+        self.priority = priority
+        self.title = title
+        self.description = description
+        self.upvotes = 0
+        self.downvotes = 0
