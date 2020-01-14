0ec786c6de1dac41579b83cbadc9d3587296ef4c
Clement6Chan <cchan00@stuy.edu>
2020-01-13 23:00:46 -0500

    update modals

--- app/static/js/myPopover.js
+++ app/static/js/myPopover.js
@@ -6,8 +6,8 @@ $(document).ready(function(){
         placement : 'top',
         html : true,
         title : 'User Info <a href="#" class="close" data-dismiss="alert">&times;</a>',
-        content : '<button  class="btn btn-info" data-toggle="modal" data-target="#myModal1">Open Modal1</button><br>'+
-            '<button  class="btn btn-primary" data-toggle="modal" data-target="#myModal2">Open Modal2</button>'
+        content : '<button  class="btn btn-danger" data-toggle="modal" data-target="#taskModal">Task</button><br>'+
+            '<button  class="btn btn-warning" data-toggle="modal" data-target="#eventModal">Event</button>'
 
     });
 });
